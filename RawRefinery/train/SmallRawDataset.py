ce0b791ff4472a0a5ad7917ef2b5bae1bbb283a3
Ryan Mueller <ryan.dalrymple.mueller@gmail.com>
2025-09-10 21:01:34 -0400

    Added small dataset helper class

---
+++ RawRefinery/train/SmallRawDataset.py
@@ -0,0 +1,82 @@
+import pandas as pd
+import os
+from  torch.utils.data import Dataset
+import imageio
+from colour_demosaicing import (
+    ROOT_RESOURCES_EXAMPLES,
+    demosaicing_CFA_Bayer_bilinear,
+    demosaicing_CFA_Bayer_Malvar2004,
+    demosaicing_CFA_Bayer_Menon2007,
+    mosaicing_CFA_Bayer)
+
+from RawRefinery.utils.image_utils import cfa_to_sparse, inverse_gamma_tone_curve
+import numpy as np
+import torch 
+import cv2
+
+class SmallRawDataset(Dataset):
+    def __init__(self, path, csv, crop_size=180, buffer=10):
+        super().__init__()
+        self.df = pd.read_csv(os.path.join(path, csv))
+        self.path = path
+        self.crop_size = crop_size
+        self.buffer = buffer
+        self.coordinate_iso = 6400
+
+    def __len__(self):
+        return len(self.df)
+    
+    def __getitem__(self, idx):
+        row = self.df.iloc[idx]
+
+        # Get Row Matrix
+        shape=(2,3)
+        cols = [f"m{i}{j}" for i in range(shape[0]) for j in range(shape[1])]
+        flat = np.array([row.pop(c) for c in cols], dtype=np.float32)
+        warp_matrix = flat.reshape(shape)
+        warp_matrix
+
+        # Load images
+        with imageio.imopen(f"{self.path}/{row.noisy_image}_bayer.jpg", "r") as image_resource:
+            bayer_data = image_resource.read()
+
+        with imageio.imopen(f"{self.path}/{row.gt_image}.jpg", "r") as image_resource:
+            gt_image = image_resource.read()
+
+        #Crop images
+        h, w, _ = gt_image.shape
+        top = np.random.randint(0 + self.buffer, h - self.crop_size - self.buffer)
+        if top % 2 != 0: top = top - 1
+        left = np.random.randint(0 + self.buffer, w - self.crop_size - self.buffer)
+        if left % 2 != 0: left = left - 1
+        bottom = top + self.crop_size
+        right = left + self.crop_size
+        gt_image = gt_image[top:bottom, left:right]
+        bayer_data = bayer_data[top:bottom, left:right]
+        h, w, _ = gt_image.shape
+
+        gt_image = gt_image/255
+        bayer_data = bayer_data/255
+
+        # Translate to linear
+        gt_image = inverse_gamma_tone_curve(gt_image)
+        bayer_data = inverse_gamma_tone_curve(bayer_data)
+
+        demosaiced_noisy = demosaicing_CFA_Bayer_Malvar2004(bayer_data)
+        sparse, _ = cfa_to_sparse(bayer_data)
+        rggb = bayer_data.reshape(h // 2, 2, w // 2, 2, 1).transpose(3, 1, 4, 0, 2).reshape(4, h // 2, w // 2)
+        
+        # Align GT
+        gt = cv2.warpAffine(gt_image, warp_matrix, (w, h), flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
+
+        # Convert to tensors
+        output = {
+            "bayer": torch.tensor(bayer_data).to(float), 
+            "gt": torch.tensor(gt).to(float).permute(2, 0, 1), 
+            "sparse": torch.tensor(sparse).to(float),
+            "noisy": torch.tensor(demosaiced_noisy).to(float).permute(2, 0, 1), 
+            "rggb": torch.tensor(rggb).to(float),
+            "conditioning": torch.tensor([row.iso/self.coordinate_iso]).to(float), 
+        }
+
+        return output
--- RawRefinery/utils/image_utils.py
+++ RawRefinery/utils/image_utils.py
@@ -168,9 +168,7 @@ def bilinear_demosaic(sparse, pattern="RGGB", cfa_type="bayer"):
     return rgb
 
 
-def inverse_gamma_tone_curve(img: np.ndarray, gamma: float = 2.2) -> np.ndarray:
-    img = np.clip(img, 0, 1)  
-    return np.power(img, gamma)
+
 
 
 def bilinear_demosaic_torch(sparse, pattern="RGGB", cfa_type="bayer"):
@@ -223,3 +221,69 @@ def bilinear_demosaic_torch(sparse, pattern="RGGB", cfa_type="bayer"):
         rgb[ch] = F.conv2d(F.pad(x, (pad_w, pad_w, pad_h, pad_h), mode="reflect"), k)[0,0]
 
     return rgb
+
+def cfa_to_sparse(image, pattern="RGGB", cfa_type="bayer"):
+    """
+    Make a sparse representation from a CFA
+
+    Args:
+        image: numpy array (H, W), RGB image in [0, 1] or [0, 255].
+        pattern: CFA pattern string, one of {"RGGB","BGGR","GRBG","GBRG"} for Bayer,
+                 or ignored if cfa_type="xtrans".
+        cfa_type: "bayer" or "xtrans".
+
+    Returns:
+        cfa: numpy array (r, H, W), sparse CFA image.
+        sparse_mask:  numpy array (r, H, W), mask of pixels.
+    """
+    H, W= image.shape
+    cfa = np.zeros((3, H, W), dtype=image.dtype)
+    sparse_mask = np.zeros((3, H, W), dtype=image.dtype)
+    if cfa_type == "bayer":
+        # 2×2 Bayer masks
+        masks = {
+            "RGGB": np.array([["R", "G"], ["G", "B"]]),
+            "BGGR": np.array([["B", "G"], ["G", "R"]]),
+            "GRBG": np.array([["G", "R"], ["B", "G"]]),
+            "GBRG": np.array([["G", "B"], ["R", "G"]]),
+        }
+        if pattern not in masks:
+            raise ValueError(f"Unknown Bayer pattern: {pattern}")
+
+        mask = masks[pattern]
+        cmap = {"R": 0, "G": 1, "B": 2}
+         
+        for i in range(2):
+            for j in range(2):
+                ch = cmap[mask[i, j]]
+                cfa[ch, i::2, j::2] = image[i::2, j::2]
+                sparse_mask[ch, i::2, j::2] = 1
+    elif cfa_type == "xtrans":
+        # Fuji X-Trans 6×6 repeating pattern
+        xtrans_pattern = np.array([
+            ["G","B","R","G","R","B"],
+            ["R","G","G","B","G","G"],
+            ["B","G","G","R","G","G"],
+            ["G","R","B","G","B","R"],
+            ["B","G","G","R","G","G"],
+            ["R","G","G","B","G","G"],
+        ])
+        cmap = {"R":0, "G":1, "B":2}
+
+        for i in range(6):
+            for j in range(6):
+                ch = cmap[xtrans_pattern[i, j]]
+                cfa[ch, i::6, j::6] = image[i::6, j::6]
+                sparse_mask[ch, i::2, j::2] = 1
+    else:
+        raise ValueError(f"Unknown CFA type: {cfa_type}")
+
+    return cfa, sparse_mask
+
+
+def apply_gamma(x, gamma=2.2):
+    return x ** (1 / gamma)
+
+def inverse_gamma_tone_curve(img: np.ndarray, gamma: float = 2.2) -> np.ndarray:
+    img = np.clip(img, 0, 1)  
+    return np.power(img, gamma)

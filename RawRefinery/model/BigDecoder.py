47929d1901fb52e00eb68ef279cda4b87965bf30
Ryan Mueller <ryan.dalrymple.mueller@gmail.com>
2025-09-13 18:20:51 -0400

    Added Heirarchical model for comparison, used in test for "BigDecoder.pt" and "HBlockModel.pt"

--- RawRefinery/model/BigDecoder.py
+++ RawRefinery/model/BigDecoder.py
@@ -1,4 +1,4 @@
-from RawRefinery.model.Cond_NAFNet import ConditionedChannelAttention, SimpleGate
+from RawRefinery.model.Cond_NAFNet import ConditionedChannelAttention, SimpleGate, NAFBlock0
 import torch
 import torch.nn as nn
 import numpy as np
@@ -16,6 +16,7 @@ class LKA(nn.Module):
         x = self.large_conv(x)
         x = self.out_conv(x)
         return x 
+    
 class GatedBottleNeck(nn.Module):
     def __init__(self, chan, ratio=2, cond_chans=0, r=16):
         super().__init__()
@@ -47,17 +48,30 @@ class GatedBottleNeck(nn.Module):
 
         return (in_x + self.beta*x, in_cond)
     
-
+class Fuser(nn.Module):
+    def __init__(self, chan):
+        super().__init__()
+        self.pwconv = nn.Conv2d(chan * 2, chan * 2, 1, 1, 0)
+        #self.NKA = NKA(chan * 2)
+        self.sg = SimpleGate()
+    def forward(self, x1, x2):
+        x = torch.cat([x1, x2], dim=1)
+        x = self.pwconv(x)
+        #x = self.NKA(x)
+        x = self.sg(x)
+        return x
+    
 class ScaleAndProcess(nn.Module):
-    def __init__(self, in_chan, width, out_chan, n_blocks, scale, cond_chans=1):
+    def __init__(self, in_chan, width, out_chan, n_blocks, scale, cond_chans=1, ):
         super().__init__()
         assert np.log(scale)/np.log(2) == int(np.log(scale)/np.log(2)), f"Scale ({scale}) must be a multiple of 2"
         padding = max(scale // 2 - 1, 0)
         self.scale = scale
         self.in_conv = nn.Conv2d(in_chan, width, scale, scale, padding)
         self.blocks = nn.Sequential(
-            *[BottleNeck(width, cond_chans=cond_chans) for _ in range(n_blocks)]
+            *[NAFBlock0(width, cond_chans=cond_chans) for _ in range(n_blocks)]
         )
+        self.fuser = Fuser(width)
         self.out_conv = nn.Conv2d(width, out_chan, 1, 1, 0)
     
     def forward(self, input):
@@ -67,7 +81,7 @@ class ScaleAndProcess(nn.Module):
             in_image, cond, previous_output = input
         after_conv = self.in_conv(in_image)
         if len(input) == 3:
-            after_conv = after_conv + previous_output
+            after_conv = self.fuser(after_conv, previous_output)
             
         output = self.blocks((after_conv, cond))[0]
         
@@ -120,4 +134,82 @@ class MultiScaleLoss(nn.Module):
         for output in outputs:
             scaled = nn.functional.interpolate(gt, size=output.shape[-2:])
             loss += self.loss_func(output, scaled)
-        return loss
+        return loss/len(outputs)
+    
+
+
+
+##
+## Hierarchical
+##
+
+class StepDownBlock(nn.Module):
+    def __init__(self, chan, scale, block, n_block, cond_chans=1):
+        super().__init__()
+        padding = max(scale // 2 - 1, 0)
+        self.down = nn.Conv2d(chan, chan, scale, scale, padding)
+        self.block = nn.Sequential(*[block(chan, cond_chans=cond_chans) for _ in range(n_block)])
+        self.up = nn.Sequential(nn.Conv2d(chan, chan * scale ** 2, 1, 1, 0), nn.PixelShuffle(scale))
+        self.beta = nn.Parameter(torch.zeros(1, chan, 1, 1), requires_grad=True)
+    def forward(self, input):
+        x = input[0]
+        cond = input[1]
+        x = self.down(x)
+        x = self.block((x, cond))[0]
+        x = self.up(x)
+        # Residual
+        x = self.beta * x + input[0]
+        return (x, cond)
+    
+
+class HierarchicalBlock(nn.Module):
+    def __init__(self, chan, scales=[8, 4, 2, 1], n_blocks=[1, 1, 1, 1], block=NAFBlock0, cond_chans=1):
+        super().__init__()
+        self.stages = nn.ModuleList()
+        for scale, n_block in zip(scales, n_blocks):
+            if scale!=1:
+                padding = max(scale // 2 - 1, 0)
+                self.stages.append(StepDownBlock(chan, scale, block, n_block, cond_chans=cond_chans))
+            else:
+                self.stages.append(block(chan, cond_chans=cond_chans))
+    
+    def forward(self, input):
+        x = input[0]
+        cond = input[1]
+        
+        for stage in self.stages:
+            x, _ = stage( (x, cond))
+            
+        return (x, cond)
+
+
+class HBlockModel(nn.Module):
+    def __init__(self, chan, width, n_hblocks, scales=[8, 4, 2, 1],  n_blocks=[2, 1, 1, 1], block=NAFBlock0):
+        super().__init__()
+        self.in_conv = nn.Conv2d(chan, width, 3, 1, 1)
+        self.blocks = nn.ModuleList()
+        for i in range(n_hblocks):
+            self.blocks.append(HierarchicalBlock(width, scales=scales,  n_blocks=n_blocks, block=block))
+            self.out_convs = nn.Conv2d(width, chan, 3, 1, 1)
+ 
+    def forward(self, x, cond):
+        inp = x
+        x = self.in_conv(x)
+        outputs = []
+        for block in self.blocks:
+            x = block( (x, cond) )[0]
+            outputs.append(self.out_convs(x))
+        return outputs
+    
+
+class SlapTwoBigDecoders(nn.Module):
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        self.decoder1 = BigDecoder(*args, **kwargs)
+        self.decoder2 = BigDecoder(*args, **kwargs)
+    def forward(self, input):
+        x = input[0]
+        cond = input[1]
+        out1 = self.decoder1( x, cond)
+        out2 = self.decoder2( out1[-1], cond)
+        return out1 + out2

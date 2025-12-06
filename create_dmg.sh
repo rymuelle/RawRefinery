004d7d579fbadb90654156823e16ed29435da75c
Ryan Mueller <ryan.dalrymple.mueller@gmail.com>
2025-12-05 19:12:20 -0500

    prep for merge

---
+++ create_dmg.sh
@@ -0,0 +1,8 @@
+create-dmg \
+  --volname "RawRefinery Installer" \
+  --window-pos 200 120 \
+  --window-size 600 400 \
+  --icon-size 100 \
+  --app-drop-link 425 200 \
+  "dist/RawRefinery.dmg" \
+  "dist/RawRefinery.app"
